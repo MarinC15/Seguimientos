4467b6769339dbc49bb38dba1230b200758e9dfe
MarinC15 <cesarmarin118029@correo.itm.edu.co>
2024-11-03 16:13:51 -0500

    eliminacion proyecto viejo

--- Segumientos.TaskList/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Segumientos.TaskList.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
